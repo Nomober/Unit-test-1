110cc1a951f7d0f88a64d1cce3d85815ac11da3b
Elmar <erimihanov@mail.ru>
2024-12-22 22:49:59 +0300

    add expect in test :)

--- src/js/__tests__/basic.js
+++ src/js/__tests__/basic.js
@@ -2,12 +2,13 @@ import status from 'statuses'
 import healthBar from '../app'
 
 test.each([
-    ['Маг', 50],
-    ['Воин', 33],
-    ['Лучник', 5]
+    ['Маг', 50, 'healthy'],
+    ['Воин', 33, 'wounded'],
+    ['Лучник', 5, 'critical']
 ])
 (
-    'testing function healthBar on %s with %i health points ', (_, health) => {
+    'testing function healthBar on %s with %i health points ', (_, health, expected) => {
         const result = healthBar(health)
+        expect(result).toBe(expected)
     }
 )
